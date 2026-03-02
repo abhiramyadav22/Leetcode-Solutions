1e8bd6def8ccfdf24d7b7543ad00d534d52dae69
Abhiram Yadav <154219077+abhiramyadav22@users.noreply.github.com>
2026-03-02 12:59:33 +0530

    Time: 2 ms (96.23%), Space: 49.8 MB (33.96%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.java
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.java
@@ -1,36 +1,32 @@
 class Solution {
     public int minSwaps(int[][] grid) {
+
         int n = grid.length;
-        
-        int[] trailing = new int[n];
+        int[] zeros = new int[n];
         for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = n - 1; j >= 0; j--) {
-                if (grid[i][j] == 0)
-                    count++;
-                else
-                    break;
+            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--) {
+                zeros[i]++;
             }
-            trailing[i] = count;
         }
         int swaps = 0;
         for (int i = 0; i < n; i++) {
-            int required = n - 1 - i;
+
+            int need = n - 1 - i;
             int j = i;
-            
-            while (j < n && trailing[j] < required)
+
+            while (j < n && zeros[j] < need) {
                 j++;
-            if (j == n)
-                return -1;
-            
+            }
+            if (j == n) return -1;
             while (j > i) {
-                int temp = trailing[j];
-                trailing[j] = trailing[j - 1];
-                trailing[j - 1] = temp;
+                int temp = zeros[j];
+                zeros[j] = zeros[j - 1];
+                zeros[j - 1] = temp;
                 swaps++;
                 j--;
             }
         }
+
         return swaps;
     }
 }
